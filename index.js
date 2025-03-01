03ef3f9b3a77161fcb162a3d32d296e85847bc98
hazerd-azerd <atmajahafizh9990@gmail.com>
2025-03-01 09:44:44 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+{
+  "name": "hazerdbot",
+  "version": "1.0.0",
+  "description": "WhatsApp bot with 300+ features, built by Hazerd Azerd.",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "dev": "nodemon index.js"
+  },
+  "author": "Hazerd Azerd",
+  "license": "MIT",
+  "dependencies": {
+    "whatsapp-web.js": "^1.20.0",
+    "qrcode-terminal": "^0.12.0",
+    "axios": "^1.4.0",
+    "dotenv": "^16.3.1",
+    "fs-extra": "^11.1.1",
+    "express": "^4.18.2"
+  },
+  "devDependencies": {
+    "nodemon": "^3.0.1"
+  },
+  "engines": {
+    "node": ">=14"
+  }
+}
